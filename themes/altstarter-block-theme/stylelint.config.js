f15dfb7ce8fc05835c9185592e2cddeb1bd49e76
Amber Alter <amber@altersego.net>
2025-04-17 17:01:04 -0700

    fix stylelint issues

--- themes/altstarter-block-theme/stylelint.config.js
+++ themes/altstarter-block-theme/stylelint.config.js
@@ -1,6 +1,13 @@
 const config = {
 	extends: ["stylelint-config-standard-scss", "@10up/stylelint-config"],
-	rules: {},
+	rules: {
+		"at-rule-no-unknown": [
+			true,
+			{
+				ignoreAtRules: ["use", "forward", "import", "mixin"],
+			},
+		],
+	},
 };
 
 module.exports = config;
